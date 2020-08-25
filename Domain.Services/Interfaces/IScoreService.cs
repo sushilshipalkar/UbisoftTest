6d5f9877144f3fdf85c0b5982759f94624f09746
sushilshipalkar <sushil.shipalkar2012@gmail.com>
2020-08-25 15:11:07 +0530

    Iservice change

--- Domain.Services/Interfaces/IScoreService.cs
+++ Domain.Services/Interfaces/IScoreService.cs
@@ -12,6 +12,7 @@ namespace Domain.Services.Interfaces
         List<ScoreDTO> getScoreByTime(DateTime playDateTime, string matchName);
         List<ScoreDTO> getScoreById(int id);
         int insertScore(ScoreDTO playerData);
-ScoreDTO getPlayerById(int id);
+
+	ScoreDTO getPlayerById(int id);
     }
 }

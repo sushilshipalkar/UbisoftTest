c1c9020bfaca81812a5ad0039fbbaeb85945de30
sushilshipalkar <sushil.shipalkar2012@gmail.com>
2020-08-25 17:57:58 +0530

    score get method added

--- WebApplication2/Controllers/ScoreController.cs
+++ WebApplication2/Controllers/ScoreController.cs
@@ -118,19 +118,52 @@ namespace WebApplication2.Controllers
             return id;
         }
 
-        // GET: api/Score
-        [HttpGet]
-        public IEnumerable<string> Get()
+	[HttpGet]
+        public ScoreDTO Get(int id)
         {
-            return new string[] { "value1", "value2" };
+             ScoreDTO model= new ScoreDTO();
+
+            List<ScoreDTO> scoreList = new List<ScoreDTO>() {
+                new ScoreDTO() { ScoreId = 1, matchName="csGo", userName = "abc", rank = 1, score = 400,createdBy = "abc", kills = 20, CreatedDate = DateTime.Parse("2020-07-09 08:35:04.377")},
+                new ScoreDTO() {ScoreId =  2,matchName="csGo", userName = "xyz", rank = 2, score = 320,createdBy = "xyz", kills = 16, CreatedDate = DateTime.Parse("2020-07-09 08:35:04.377") },
+                new ScoreDTO() { ScoreId = 3,matchName="csGo", userName = "suresh", rank = 3, score = 200,createdBy = "suresh", kills = 10, CreatedDate = DateTime.Parse("2020-07-09 08:35:04.377") },
+                new ScoreDTO() { ScoreId = 4,matchName="pubG", userName = "asd", rank = 5, score = 100,createdBy = "asd", kills = 5, CreatedDate = DateTime.Parse("2020-07-09 08:35:04.377") },
+                new ScoreDTO() { ScoreId = 5,matchName="csGo", userName = "zxc", rank = 4, score = 300,createdBy = "zxc", kills = 15, CreatedDate = DateTime.Parse("2020-07-09 08:35:04.377")},
+                new ScoreDTO() { ScoreId = 6,matchName="csGo", userName = "asd", rank = 2, score = 100,createdBy = "asd", kills = 5, CreatedDate = DateTime.Parse("2020-07-09 08:36:04.377") },
+                new ScoreDTO() { ScoreId = 7,matchName="csGo", userName = "zxc", rank = 1, score = 300,createdBy = "zxc", kills = 15, CreatedDate = DateTime.Parse("2020-07-09 08:36:04.377")}
+            };
+            try
+            {
+                model = scoreList.FirstOrDefault(x => x.ScoreId == id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return model;
+
+
+
         }
 
+
+
+        // GET: api/Score
+       // [HttpGet]
+       // public IEnumerable<string> Get()
+       // {
+       //     return new string[] { "value1", "value2" };
+       // }
+
         // GET: api/Score/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //[HttpGet("{id}", Name = "Get")]
+       // public string Get(int id)
+       // {
+        //    return "value";
+        //}
+
         
         // POST: api/Score
         [HttpPost]
